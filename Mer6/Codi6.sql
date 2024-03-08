0a6a1781307c5ea46f12134921a7fba933d7e331
AlvaroSintes <148210787+AlvaroSintes@users.noreply.github.com>
2024-03-08 09:03:58 +0100

    Add files via upload

---
+++ Mer6/Codi6.sql
@@ -0,0 +1,3 @@
+use MER6;
+INSERT INTO Reserves ( ID, DATA , Import, Num_asiento, Vol_ID, Passatger_ID)
+VALUE ( '00015', '2016-6-9', '130', 'A2', '5', '5') 
---
+++ Mer6/MER6consulta5.sql
@@ -0,0 +1,4 @@
+use MER6;
+SELECT Reserves.Data, Reserves.Import, Vol.Num_vol, Passatger.Nom, Passatger.Cognom
+FROM Reserves, Vol, Passatger
+WHERE Passatger.ID=Reserves.Passatger_ID and Vol.ID=Reserves.Vol_ID and Passatger.Nom='Oscar';
